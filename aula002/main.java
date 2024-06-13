4f9ab879780dff28c06a65f4722e9c7d7d0c60b4
Nathan Albuquerque <nathan.nra@outlook.com>
2024-06-13 16:14:27 -0300

    aula002 - operacoes basicas

---
+++ aula002/main.java
@@ -0,0 +1,23 @@
+package br.com.letscode.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int a;
+        int b = 2;
+        a = 3
+
+        int soma = a + b;
+        int subtracao = a - b;
+        int multplicacao = a * b;
+        // realizando cast para transformar a tipagem e receber o resultado esperado
+        float divisao = (float) a / b;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multplicacao);
+        System.out.println(divisao);
+
+    }
+}
