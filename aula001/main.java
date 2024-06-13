90405cdb9102358acc02d4a40c83ed8f2595438e
Nathan Albuquerque <nathan.nra@outlook.com>
2024-06-13 16:13:20 -0300

    aula001 - introducao a variaveis

---
+++ aula001/main.java
@@ -0,0 +1,13 @@
+package br.com.letscode.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        String nome = "Nathan";
+        nome = "Pessoa";
+        nome = "Alguem";
+        System.out.println("Ola, " + nome);
+    }
+}
